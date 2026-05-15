5f6059b5681c621ff5a1e5f283162958a8763081
moz <moziar@outlook.com>
2026-05-15 14:02:40 +0800

    chore: rename couple of icons

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import type { IconCategories, ExternalLinksIconSettings } from './types';
 export const ICON_CATEGORIES: IconCategories = {
 	URL_SCHEME: [
 		'goodlinks', 'zotero', 'snippetslab', 'siyuan', 'eagle', 
-		'bear', 'prodrafts', 'things', 'shortcuts', 'file', 'craftdocs', 'obsidiannote', 'advancedurisetting'
+		'bear', 'prodrafts', 'things', 'shortcuts', 'file', 'craft', 'obsidiannote', 'advancedurisetting'
 	] as const,
 	WEB: {
 		'github': 'github.com',
